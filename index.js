97e4e00339cfc89317df5233a4a889a2b886ef59
HGiang01 <nguyenhoanggiang090804@gmail.com>
2025-09-15 16:13:34 +0700

    refactor: Convert to ES module format & change static router / to /public

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-// index.js
-// where your node app starts
+// Import required modules
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import express from 'express';
+import cors from 'cors';
 
-// init project
-var express = require('express');
+// Init constants and app
+// Resolve file/dir paths (ES modules don't have __filename/__dirname by default)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
+// Set up middlewares
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use('/public', express.static(path.join(process.cwd(), '/public')));
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+// Define routers
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get('/api/hello', (req, res) => {
   res.json({greeting: 'hello API'});
 });
 
-
-
-// Listen on port set in environment variable or default to 3000
+// Start the server
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
